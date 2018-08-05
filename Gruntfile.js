77e0e33f7c735dc3a027469e35ebc41eb9cb7640
Adam Carter <carter.c.adam@gmail.com>
2018-08-04 21:04:30 -0700

    complete proxy on local host

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,84 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
+
+    concat: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        separator: ';',
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      dist: {
+        src: [
+          'reservation-service/public/bundle.js',
+          'hero-photo-service/client/dist/bundle.js',
+          'Review-Service/client/dist/app.js',
+          'about-service/public/app.js'
+        ],
+        dest: 'public/bundle.js'
+      },
+      // dist: {
+      //   src: [
+      //     'about-service/public/styles.css',
+      //     'hero-photo-service/client/dist/app.css',
+      //     'reservation-service/public/styles.css',
+      //     'Review-Service/client/dist/app.css'
+      //   ],
+      //   dest: 'public/bundled-styles.css'
+      // }
+    },
+
+    connect: {
+        server: {
+          options: {
+              port: 3004,
+              base: 'public',
+              hostname: 'localhost',
+              keepalive: true,
+              open: true,
+              livereload: true,
+              middleware: function (connect, options, middlewares) {
+                middlewares.unshift(require('grunt-connect-proxy/lib/utils').proxyRequest);
+                return middlewares;
+              }
+          },
+        proxies: [
+            {
+            context: '/review_service',
+            host: 'localhost',
+            port: 3002,
+            rewrite: {
+                '^/review_service':'/api'
+            }
+          },
+         {
+            context: '/hero_service',
+            host: 'localhost',
+            port: 3000,
+            rewrite: {
+                '^/hero_service':'/'
+            }
+          },
+          {
+            context: '/about_service',
+            host: 'localhost',
+            port: 3001,
+            rewrite: {
+                '^/about_service':'/'
+            }
+          },
+        ]
       }
     }
   });
 
-    // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-connect-proxy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.registerTask('default', ['concat']);
+
+  grunt.registerTask('server', function(target) {
+    grunt.task.run([
+      'configureProxies:server',
+      'connect:server',
+    ]);
+  });
+}
